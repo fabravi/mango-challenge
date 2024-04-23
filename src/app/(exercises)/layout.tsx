890af7d224192793566fd04843fc1bbe0668ba4d
Facundo Bravi <fabravi@gmail.com>
2024-04-23 21:12:24 +0200

    Create app layout

--- src/app/(exercises)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/globals.scss";
-
-export default function ExercisesLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <main className="main">{children}</main>;
-}
